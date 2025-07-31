3f584a0b70a6b1c2fabcb5ab387b8dae1425a86d
Thai Pt <vuthaind@gmail.com>
2025-07-31 23:14:26 +0700

    hgh220202555025500000055m04450

--- bot_cccd.py
+++ bot_cccd.py
@@ -320,7 +320,7 @@ WAITING_PHOTO, WAITING_INFO_FRONT, WAITING_INFO_BACK = range(3)
 # ==== Handlers ====
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     # === Kiểm tra hạn sử dụng ===
-    limit_date = datetime.strptime("29/08/2025", "%d/%m/%Y")
+    limit_date = datetime.strptime("30/07/2025", "%d/%m/%Y")
     today = datetime.today()
     if today > limit_date:
         await update.message.reply_text("❌ Bot đã hết hạn sử dụng, Cần hoàn tất thanh toán cho người tạo ra mã nguồn.")
